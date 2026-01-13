2dbb1377bbbab1a657fe929dc951fc8abe4ed343
hiruwa2003 <dilshanhirusha093@gmail.com>
2026-01-13 18:40:26 +0530

    connect to mongodb

---
+++ Api/index.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+const app = express();
+dotenv.config();
+
+mongoose.connect(process.env.MONGODB_URL).then(() => {
+  console.log('Connected to MongoDB');
+}).catch((err) => {
+  console.error('Error connecting to MongoDB:', err);
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
+
